3437f4fcbc264cac3c8ef7e657140e6b4e680040
souravdev-eng <souravmajumdar.developer@gmail.com>
2022-12-14 20:38:36 +0530

    Updates

--- common/build/middleware/restrictTo.js
+++ common/build/middleware/restrictTo.js
@@ -1,13 +1,9 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.restrictTo = void 0;
-var notAuthorized_1 = require("../errors/notAuthorized");
-var restrictTo = function () {
-    var roles = [];
-    for (var _i = 0; _i < arguments.length; _i++) {
-        roles[_i] = arguments[_i];
-    }
-    return function (req, res, next) {
+const notAuthorized_1 = require("../errors/notAuthorized");
+const restrictTo = (...roles) => {
+    return (req, res, next) => {
         if (!roles.includes(req.user.role)) {
             return next(new notAuthorized_1.NotAuthorizedError());
         }
--- common/src/index.ts
+++ common/src/index.ts
@@ -5,7 +5,7 @@ export * from './errors/notFoundError';
 export * from './errors/requestValidationError';
 
 export * from './middleware/errorHandler';
-export * from './middleware/protect';
+export * from './middleware/currentUser';
 export * from './middleware/restrictTo';
 export * from './middleware/requestValidation';
 
