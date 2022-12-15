a42741e27b996d36fa9dbe546a0ce20edb8d3cb0
souravdev-eng <souravmajumdar.developer@gmail.com>
2022-12-15 21:32:19 +0530

    Updates

--- common/src/middleware/errorHandler.ts
+++ common/src/middleware/errorHandler.ts
@@ -2,9 +2,9 @@ import { NextFunction, Request, Response } from 'express';
 import { BaseError } from '../errors/baseError';
 
 export const errorHandler = async (err: Error, req: Request, res: Response, next: NextFunction) => {
-  if (process.env.NODE_ENV !== 'test') {
-    console.log(err.stack);
-  }
+  // if (process.env.NODE_ENV !== 'test') {
+  console.log(err.stack);
+  // }
   if (err instanceof BaseError) {
     return res.status(err.statusCode).json({ errors: err.serializeErrors() });
   }
