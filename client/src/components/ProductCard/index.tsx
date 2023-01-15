18e1b4b2f97dc01b53935560b703e19542fb9dc2
souravdev-eng <souravmajumdar.developer@gmail.com>
2023-01-15 20:08:30 +0530

    adding product details page

--- client/src/components/ProductCard/index.tsx
+++ client/src/components/ProductCard/index.tsx
@@ -1,23 +1,27 @@
 import { FC } from 'react';
 import { Rating } from '@mui/material';
+import { Link } from 'react-router-dom';
 import './product-card.style.scss';
 
 interface CardProps {
+  id: string;
   title: string;
   price: number;
   image: string;
   rating: number;
 }
 
-const ProductCard: FC<CardProps> = ({ title, price, image, rating }) => {
+const ProductCard: FC<CardProps> = ({ id, title, price, image, rating }) => {
   return (
     <div className='card'>
-      <img className='card__image' src={image} alt={title} />
-      <h3 className='card__title'>{title}</h3>
-      <div className='card__info'>
-        <span className='card__info__price'>₹{price}</span>
-        <Rating name='half-rating-read' defaultValue={rating} precision={0.5} readOnly />
-      </div>
+      <Link to={`/product/${id}`} style={{ textDecoration: 'none' }}>
+        <img className='card__image' src={image} alt={title} />
+        <h3 className='card__title'>{title}</h3>
+        <div className='card__info'>
+          <span className='card__info__price'>₹{price}</span>
+          <Rating name='half-rating-read' defaultValue={rating} precision={0.5} readOnly />
+        </div>
+      </Link>
     </div>
   );
 };
--- client/src/pages/ProductDetails/index.tsx
+++ client/src/pages/ProductDetails/index.tsx
@@ -1,11 +1,35 @@
 import React from 'react';
+import { Rating } from '@mui/material';
 import { useParams } from 'react-router-dom';
+import ProductDetails from '../../data/productDetails.json';
+import './product-details.style.scss';
 
 const ProductDetail = () => {
   let { id } = useParams();
+
   return (
-    <div>
-      <h2>ProductDetail {id}</h2>
+    <div className='container'>
+      <div className='productInfo'>
+        <img src={ProductDetails.image} className='productInfo__image' />
+        <div>
+          <h2 className='productInfo__title'>{ProductDetails.title}</h2>
+          <Rating
+            name='half-rating-read'
+            defaultValue={ProductDetails.rating}
+            precision={0.5}
+            readOnly
+            size='large'
+          />
+          <div className='productDetails'>
+            <h5 className='productDetails__price'>₹{ProductDetails.price}</h5>
+            <span className='productDetails__description'>{ProductDetails.description}</span>
+          </div>
+          <div className='buttonContainer'>
+            <button className='btn btn-outline'>Add to cart</button>
+            <button className='btn btn-fill'>Buy Now</button>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
